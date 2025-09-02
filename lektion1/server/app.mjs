d7ca25ef539df9a9214fb315e955e88842615cbc
oliver.granasjungdalen <oliver.granasjungdalen@gmail.com>
2025-09-02 11:40:42 +0000

    (add router)

--- lektion1/server/app.mjs
+++ lektion1/server/app.mjs
@@ -2,13 +2,36 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
-const port = 3000
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+
 
 app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false })) 
+
+const saveMessage = () => {
+     fs.writeFilesync(fileUrlToPatth), JSON.stringify(messages)
+}
+
+
+
+app.post("/messages", (req,res) => {
+    const {name,message} = req.body  
+    
+    try {
+        const newMessage = {
+            name,
+            message,
+            timestamp: new Date().toISOString()
+        }
+    
+        saveMessage(message)
+        res.statusCode(201).json("saved successfully")
+    } catch (error) {
+        console.log(error)
+        res.status(500).json("internal server error")
+    }
+    
+    
+})
 
 export default app 
