bc5b05b84545c4f2011058565549be0d9b168382
oliver.granasjungdalen <oliver.granasjungdalen@gmail.com>
2025-09-08 08:54:19 +0000

    [add] check if inputs contains data

--- lektion1/klient/index.js
+++ lektion1/klient/index.js
@@ -1,3 +1,37 @@
 const form = document.querySelector(".input-form")
+const submitBtn = document.querySelector(".button[type= 'submit']")
+
+let inputname = form.elements.name.value
+let inputmessage = form.elements.message.value
+form.addEventListener("input", checkInputs)
+
+
+
+
+const checkInputs = () => {
+    const inputname = form.elements.name.value
+    const inputmessage = form.elements.message.value
+    
+
+   if (!inputname  || !inputmessage) submitBtn.disabled = true 
+   else submitBtn.disabled = false
+}
+
+
+form.addEventListener("submit", async (e) => { // e är förkortning för e
+    e.preventDefault() // hindrar formuläret från att ladda om
+
+
+    const name = form.elements.name.value // läser in det som skrivs i namn
+    const message = form.elements.message.value
+
+    
+
+    if (!name  || !message) return alert("fyll i båda fälten!")
+
+
+
+
+    })
 
 
