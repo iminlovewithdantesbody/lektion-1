fccebe62794ef3990db0749ee72c9536794f6959
oliver.granasjungdalen <oliver.granasjungdalen@gmail.com>
2025-09-09 11:58:34 +0000

    [Add]find and upptate jason file content

--- lektion1/klient/index.js
+++ lektion1/klient/index.js
@@ -1,22 +1,23 @@
 const form = document.querySelector(".input-form")
-const submitBtn = document.querySelector(".button[type= 'submit']")
+const submitBtn = document.querySelector(".button[type='submit']")
 
 let inputname = form.elements.name.value
 let inputmessage = form.elements.message.value
-form.addEventListener("input", checkInputs)
+
 
 
 
 
 const checkInputs = () => {
-    const inputname = form.elements.name.value
-    const inputmessage = form.elements.message.value
+     inputname = form.elements.name.value
+     inputmessage = form.elements.message.value
     
 
    if (!inputname  || !inputmessage) submitBtn.disabled = true 
    else submitBtn.disabled = false
 }
 
+form.addEventListener("input", checkInputs)
 
 form.addEventListener("submit", async (e) => { // e är förkortning för e
     e.preventDefault() // hindrar formuläret från att ladda om
@@ -29,9 +30,41 @@ form.addEventListener("submit", async (e) => { // e är förkortning för e
 
     if (!name  || !message) return alert("fyll i båda fälten!")
 
+        try {
+            const response = await fetch("http//localhost:3000/messages", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name:inputname,
+                    message:inputmessage
+    
+                })
+            
+               
+            }) 
 
+            if (response.ok) {
+                alert("meddelandet sparades!")
+                form.reset()
+            
+                
+            }   else {
+                alert("fel uppstod!")
+                 
+            }
+        } catch (error) {
+            console(error)
+            console.error("fel:",error)
+            alert("kunde inte skika meddelandet")
+        }
+    
+   
 
+        
 
-    })
+        
+    
 
-
+})
--- lektion1/server/app.mjs
+++ lektion1/server/app.mjs
@@ -1,15 +1,35 @@
 import express from "express"
-import cors from "cors"
+import cors from "cors"// tillåter förförfrogningar från andra domäner till vår server(cross-orgin reshrsch file)
+import fs from "fs"// node js filsyste-moudul för att läsa och skriva filler 
+import { fileURLToPath, } from "url" // hjälper oss att få sökvägefn till den aktuela filen
+import { dirname } from "path"// hjälper oss att få SÖKVÄGEN TILL den aktuela mapen
 
-const app = express()
+const __filename = fileURLToPath(import.meta.url)// hjälper oss att få den 
+const __dirname = dirname(__filename)
 
+
+
+const app = express()// skapar en express applikation
 
 app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false })) 
 
-const saveMessage = () => {
-     fs.writeFilesync(fileUrlToPatth), JSON.stringify(messages)
+const saveMessage = (messageData) => {
+ const filePath = `${__dirnamedirname}/messages.json`// skapa fullständig sökväg till jason-filen
+
+ let messages = []
+ if (fs.existsSync(filePath)) {// kollar om filen finns
+    const data = fs.readFileSync(filePath, "utf-8")// läs fill som text 
+    messages = JSON.parse(data)// konvertera json-fill till javaskript array
+
+ }
+ messages.push(messageData)// lägg till nya meddelande-objektet sist i arrayen 
+
+ // spara tillbacka hela errayen till filen
+ // jason.string() konverterar js till jason-text
+ // null,2 gör jasonfilen lättläst med identiering 
+ fs.writeFileSync(filePath, JSON.stringify(messages))
 }
 
 
